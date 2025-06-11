415211befb845109bf9406e28a9f04215251aa87
coffeee <jp131@pvsd.org>
2025-06-11 15:12:27 -0400

    c

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from typing import Union
-
+import sqlite3
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/topanime")
 async def read_root():
-    return {"Hello": "World"}
+    conn = sqlite3.connect("anime.db")
+    cur = conn.cursor()
+    f = """
+    SELECT * FROM ANIMES
+    WHERE watchcount = 3"""
+    cur.execute(f)
+    result = cur.fetchall()
+    cur.close()
+    conn.close()
+    print(result)
+    return {"result": result}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
